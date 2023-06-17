44677562cdf9f11c61a10086df7e50258cdbf073
vicodin <pcsoket@naver.com>
2023-06-17 12:55:16 +0900

    add game btn color

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -1,9 +1,17 @@
 <template>
     <h2>현재숫자: {{ currentNumber }}</h2>
     <h1>1부터 100까지 눌러주세요.</h1>
-    <div class="grid grid-cols-10 gap-2">
+    <h1>버튼색깔 {{ currentColor }}</h1>
+    <div class="grid grid-cols-10 p-2 gap-2">
+        <div v-for="(item, i) in numberBtnColorList" :key="i" class="bg-gray-200 p-2 text-center">
+            <div @click="currentColor = item.color" :class="item.color">
+                {{ item.color }}
+            </div>
+        </div>
+    </div>
+    <div class="grid grid-cols-10 p-2 gap-2">
         <div v-for="item in numberList" :key="i" class="bg-gray-200 p-2 text-center">
-            <div @click="clickNumber(item)" :class="item.clickStatus ? 'bg-red-500' : ''">
+            <div @click="clickNumber(item)" :class="item.clickStatus ? currentColor : ''">
                 {{ item.number }}
             </div>
         </div>
@@ -11,9 +19,21 @@
 </template>
 
 <script setup>
-import { ref} from 'vue'
+import { ref } from 'vue'
 
 const currentNumber = ref(0);
+const currentColor = ref('red');
+
+//check 시 컬러리스트 check 클릭시 true 나머지 false
+const numberBtnColorList = ref([
+    { color: 'bg-red-200', check: false },
+    { color: 'bg-blue-200', check: false },
+    { color: 'bg-yellow-200', check: false },
+    { color: 'bg-green-200', check: false },
+    { color: 'bg-purple-200', check: false },
+    { color: 'bg-orange-200', check: false },
+    { color: 'bg-pink-200', check: false },
+]);
 
 //numberList 1부터 100까지 배열
 const numberList = ref([]);
@@ -28,11 +48,9 @@ const clickNumber = (item) => {
     if (item.number !== currentNumber.value + 1) {
         return;
     } else {
-        
+        currentNumber.value = item.number;
+        item.clickStatus = true;
     }
-    
-    currentNumber.value = item.number;
-    item.clickStatus = true;
 }
 
 </script>
